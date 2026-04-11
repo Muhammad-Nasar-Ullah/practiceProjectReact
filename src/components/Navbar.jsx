efcc99c8b25f30d64fa756f599c26f3a0bad520c
Muhammad-Nasar-Ullah <nasarbutt20@gmail.com>
2026-04-11 17:19:34 +0500

    added animation to the menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,22 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import blackLogo from '../assets/Logo-black.png'
 import whiteLogo from '../assets/Logo-white.png'
+import whiteclose from '../assets/white_close.png'
+import blackclose from '../assets/black_close.png'
 
 const Navbar = ({ mode, toggleMode }) => {
 
     const [dropdown, setDropdown] = useState(false)
 
     return (
-        <div className={`px-5 py-5 relative flex justify-between items-center sticky top-0 h-[70px] z-50 transition-all duration-500 ${mode === 'dark' ? 'bg-gray-800' : 'bg-gray-200'}`}>
+        <div className={`px-5 py-5 relative flex justify-between items-center sticky top-0 h-[70px] z-50 transition-all ${!dropdown ? 'delay-250 duration-500' : 'delay-0 duration-300'} ${mode === 'dark' ? 'bg-gray-800' : 'bg-gray-200'}`}>
             <img src={mode === 'dark' ? whiteLogo : blackLogo} alt="Logo" className="h-10 w-10 sm:w-4 sm:h-4 md:w-8 md:h-8 object-contain" />
 
             <button onClick={() => setDropdown(!dropdown)} className={`px-4 py-2 rounded-md md:hidden ${mode === 'light' ? 'bg-black text-white' : 'bg-white text-black'}`} style={{ cursor: 'pointer' }}>menu</button>
 
-            <div className={`flex flex-col gap-5 fixed top-0 w-full h-[100vh] z-[100] p-5 shadow-lg border transition-all duration-300 ease-in-out ${dropdown ? 'right-0' : '-right-full'} ${mode === 'dark' ? 'bg-gray-900 border-gray-700 text-white' : 'bg-white border-gray-200 text-gray-800'}`}>
-                <button onClick={() => setDropdown(false)} className={`absolute top-3 right-3 text-sm px-2 py-1 rounded-md transition ${mode === 'dark' ? 'bg-gray-700 hover:bg-gray-600 text-white' : 'bg-gray-200 hover:bg-gray-300 text-black'}`}>
-                    ✕
+            <div className={`flex flex-col gap-5 fixed top-0 w-full h-[100vh] z-[100] p-5 shadow-lg border transition-all ease-in-out ${!dropdown ? 'delay-250 duration-300 ' : 'delay-0 duration-300'} ${dropdown ? 'right-0' : '-right-full'} ${mode === 'dark' ? 'bg-gray-900 border-gray-700 text-white' : 'bg-white border-gray-200 text-gray-800'}`}>
+                <button onClick={() => setDropdown(false)} className={`absolute top-3 right-3 text-sm px-2 py-2 rounded-md transition ${mode === 'dark' ? 'bg-gray-700 hover:bg-gray-600 text-white' : 'bg-gray-200 hover:bg-gray-300 text-black'}`}>
+                    <img src={mode === 'dark' ? whiteclose : blackclose} alt="close" className="h-6 w-6 sm:w-4 sm:h-4 md:w-8 md:h-8 object-contain" />
                 </button>
                 <ul className="flex flex-col gap-3 mt-4">
                     <li><Link to='/' onClick={() => setDropdown(false)} className={`${mode === 'dark' ? 'text-white' : 'text-black'}`}>Home</Link></li>
@@ -24,7 +26,7 @@ const Navbar = ({ mode, toggleMode }) => {
                     <li><Link to='/contact' onClick={() => setDropdown(false)} className={`${mode === 'dark' ? 'text-white' : 'text-black'}`}>Contact</Link></li>
                 </ul>
                 <div className={`${mode === 'dark' ? 'border-gray-700' : 'border-gray-200'} border-t`}></div>
-                <button onClick={toggleMode} className={`w-full py-2 rounded-lg font-medium transition duration-200 ${mode === 'dark' ? 'bg-white text-black hover:bg-gray-200' : 'bg-black text-white hover:bg-gray-800'}`}>
+                <button onClick={toggleMode} onMouseDown={() => setDropdown(false)} className={`w-full py-2 rounded-lg font-medium transition ${!dropdown ? 'delay-250 duration-200' : 'delay-0 duration-300'} ${mode === 'dark' ? 'bg-white text-black hover:bg-gray-200' : 'bg-black text-white hover:bg-gray-800'}`}>
                     {mode === 'light' ? 'Enable Dark Mode' : 'Enable Light Mode'}
                 </button>
             </div>
@@ -35,7 +37,7 @@ const Navbar = ({ mode, toggleMode }) => {
                 <li className={`${mode === 'dark' ? 'text-white' : 'text-black'}`}><Link to='/contact'>Contact</Link></li>
             </ul>
             <button onClick={toggleMode} className={`px-4 py-2 rounded-md hidden md:block ${mode === 'light' ? 'bg-black text-white' : 'bg-white text-black'}`} style={{ cursor: 'pointer' }}>{mode === 'light' ? 'Enable DarkMode' : 'Enable LightMode'}</button>
-        </div>
+        </div >
     )
 }
 
